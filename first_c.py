9366bf3f67d5c21e82e9ab47087d7b6301c0a4a2
gonzoden <deniz.nazarova.99@gmail.com>
2023-11-05 02:08:38 +0600

    first commit

---
+++ first_c.py
@@ -0,0 +1,38 @@
+"""n = input();
+#arr = list(map(int,input().split()))
+arr_n = [int(x) for x in input().split()]
+
+m = input();
+arr_m = [int(x) for x in input().split()]"""
+
+n = int(input()) 
+
+a = [int(x) for x in input().split()]
+
+m = int(input()) 
+
+b = [int(x) for x in input().split()]
+
+a_l = 0
+b_l = 0
+a_r = len(a)
+b_r = len(b)
+
+#f = [0 for _ in range(n+m)]
+f = []
+
+while (b_l <= b_r):
+	if a[a_l] <= b[b_l]:
+		f.append(a[a_l])
+		a_l += 1
+	else:
+		f.append(b[b_l])
+		b_l += 1
+
+while (a_l <= a_r):
+	f.append(a[a_l])
+	a_l += 1
+
+print(f)
+
+
