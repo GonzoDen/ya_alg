3b8ba136e457464f998cc12c49589d89d390cd19
gonzoden <deniz.nazarova.99@gmail.com>
2023-11-05 14:09:38 +0600

    update to passing code

--- first_c.py
+++ first_c.py
@@ -1,3 +1,29 @@
+def merge (a_l, a_r, b_l, b_r):
+	f = []
+	
+	while (b_l < b_r) and (a_l < a_r): #removed '<=' to avoid issues with empty arrays
+		if a[a_l] <= b[b_l]:
+			f.append(a[a_l])
+			a_l += 1
+		else:
+			f.append(b[b_l])
+			b_l += 1
+
+	while (a_l < a_r):
+			f.append(a[a_l])
+			a_l += 1
+
+	while (b_l < b_r):
+			f.append(b[b_l])
+			b_l += 1
+
+	result = " ".join(map(str, f))
+	print(result)
+	return result
+
+
+#I/O - better to create separate __init__ function
+
 n = int(input()) 
 a = [int(x) for x in input().split()]
 m = int(input()) 
@@ -5,23 +31,7 @@ b = [int(x) for x in input().split()]
 
 a_l = 0
 b_l = 0
-a_r = len(a)-1
-b_r = len(b)-1
+a_r = len(a)
+b_r = len(b)
 
-f = []
-
-while (b_l <= b_r):
-	if a[a_l] <= b[b_l]:
-		f.append(a[a_l])
-		a_l += 1
-	else:
-		f.append(b[b_l])
-		b_l += 1
-
-while (a_l <= a_r):
-	f.append(a[a_l])
-	a_l += 1
-
-print(f)
-
-
+merge(a_l, a_r, b_l, b_r)
